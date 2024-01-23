270faaa6ca05821e0c44a9746f30d8b41af1a790
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-01-22 23:41:05 -0800

    I created the project and I completed the setups for connecting to mongodb!

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Express server is running!" });
+});
+
+const dbUrl = process.env.MONGO_URL;
+const port = process.env.PORT;
+
+mongoose
+  .connect(dbUrl)
+  .then((data) => app.listen(port))
+  //   .then((data) => console.log("conneted!"))
+  .catch((err) => console.log(err));
